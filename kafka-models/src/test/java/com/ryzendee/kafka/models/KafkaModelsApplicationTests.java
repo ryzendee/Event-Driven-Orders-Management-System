771faa7249bb911ee5f0120afd64f0c3f04b59d4
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-06 16:27:24 +0400

    Init commit

---
+++ kafka-models/src/test/java/com/ryzendee/kafka/models/KafkaModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryzendee.kafka.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaModelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
