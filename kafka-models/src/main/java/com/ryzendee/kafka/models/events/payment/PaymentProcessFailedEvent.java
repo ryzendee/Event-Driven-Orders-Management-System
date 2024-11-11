74e9cef021d08bb5cca91b72ea205883166c0faa
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-11 14:53:23 +0400

    fix: Add 'productPrice' field to PaymentProcessFailedEvent

--- kafka-models/src/main/java/com/ryzendee/kafka/models/events/payment/PaymentProcessFailedEvent.java
+++ kafka-models/src/main/java/com/ryzendee/kafka/models/events/payment/PaymentProcessFailedEvent.java
@@ -4,6 +4,7 @@ package com.ryzendee.kafka.models.events.payment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Getter
@@ -11,5 +12,6 @@ import java.util.UUID;
 public class PaymentProcessFailedEvent {
     private final UUID orderId;
     private final UUID productId;
+    private final BigDecimal productPrice;
     private final Integer productQuantity;
 }
