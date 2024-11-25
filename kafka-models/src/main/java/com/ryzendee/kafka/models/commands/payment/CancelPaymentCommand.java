a9a0a30947d284fccc18dd63f656607b420dc538
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-25 13:31:05 +0400

    feat: Add compensating commands

---
+++ kafka-models/src/main/java/com/ryzendee/kafka/models/commands/payment/CancelPaymentCommand.java
@@ -0,0 +1,17 @@
+package com.ryzendee.kafka.models.commands.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class CancelPaymentCommand {
+
+    private final UUID orderId;
+    private final UUID productId;
+    private final BigDecimal productPrice;
+    private final Integer productQuantity;
+}
---
+++ kafka-models/src/main/java/com/ryzendee/kafka/models/commands/product/CancelProductReservationCommand.java
@@ -0,0 +1,15 @@
+package com.ryzendee.kafka.models.commands.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class CancelProductReservationCommand {
+
+    private final UUID orderId;
+    private final UUID productId;
+    private final Integer quantity;
+}
