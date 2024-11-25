11751521e11cb1662e12a582226d59894bb99991
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-25 13:49:04 +0400

    fix: Add orderId field

--- kafka-models/src/main/java/com/ryzendee/kafka/models/commands/order/RejectOrderCommand.java
+++ kafka-models/src/main/java/com/ryzendee/kafka/models/commands/order/RejectOrderCommand.java
@@ -3,8 +3,10 @@ package com.ryzendee.kafka.models.commands.order;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @AllArgsConstructor
 @Getter
 public class RejectOrderCommand {
-
+    private final UUID orderId;
 }
