0e55cb335e4d82142f323ad4ac5b9e50fd221758
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-25 12:28:40 +0400

    feat: Extend event handling

--- order-service/src/main/java/com/ryzendee/orderservice/saga/OrderSaga.java
+++ order-service/src/main/java/com/ryzendee/orderservice/saga/OrderSaga.java
@@ -1,7 +1,14 @@
 package com.ryzendee.orderservice.saga;
 
+import com.ryzendee.kafka.models.commands.ApproveOrderCommand;
+import com.ryzendee.kafka.models.commands.CreateShipmentCommand;
+import com.ryzendee.kafka.models.commands.ProcessPaymentCommand;
 import com.ryzendee.kafka.models.commands.ReserveProductCommand;
+import com.ryzendee.kafka.models.events.order.OrderApprovedEvent;
 import com.ryzendee.kafka.models.events.order.OrderCreatedEvent;
+import com.ryzendee.kafka.models.events.payment.PaymentProcessedEvent;
+import com.ryzendee.kafka.models.events.product.ProductReservedEvent;
+import com.ryzendee.kafka.models.events.shipment.ShipmentCreatedEvent;
 import com.ryzendee.orderservice.config.KafkaCommandTopicProperties;
 import com.ryzendee.orderservice.mapper.CommandMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -32,10 +39,35 @@ public class OrderSaga {
     }
 
     @KafkaHandler
-    public void handleOrderCreatedEvent(@Payload OrderCreatedEvent orderCreatedEvent) {
-        log.info("Received order created event: {}", orderCreatedEvent);
-        ReserveProductCommand reserveProductCommand = commandMapper.map(orderCreatedEvent);
+    public void handleOrderCreatedEvent(@Payload OrderCreatedEvent event) {
+        log.info("Received order created event: {}", event);
+        ReserveProductCommand command = commandMapper.map(event);
+        kafkaTemplate.send(topicProperties.getOrderCommandsTopic(), command);
+    }
 
-        kafkaTemplate.send(topicProperties.getOrderCommandsTopic(), reserveProductCommand);
+    @KafkaHandler
+    public void handleProductReservedEvent(@Payload ProductReservedEvent event) {
+        log.info("Received product reserved event: {}", event);
+        ProcessPaymentCommand command = commandMapper.map(event);
+        kafkaTemplate.send(topicProperties.getPaymentCommandsTopic(), command);
+    }
+
+    @KafkaHandler
+    public void handlePaymentProcessedEvent(@Payload PaymentProcessedEvent event) {
+        log.info("Received payment processed event: {}", event);
+        CreateShipmentCommand command = commandMapper.map(event);
+        kafkaTemplate.send(topicProperties.getShipmentCommandsTopic(), command);
+    }
+
+    @KafkaHandler
+    public void handleShipmentCreatedEvent(@Payload ShipmentCreatedEvent event) {
+        log.info("Received shipment created event: {}", event);
+        ApproveOrderCommand command = commandMapper.map(event);
+        kafkaTemplate.send(topicProperties.getOrderCommandsTopic(), command);
+    }
+
+    @KafkaHandler
+    public void handleOrderApprovedEvent(@Payload OrderApprovedEvent event) {
+        log.info("Received order approved event: {}", event);
     }
 }
