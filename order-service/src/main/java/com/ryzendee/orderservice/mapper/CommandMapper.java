f3eb086dcb72fc2e1187dbecc0a79f8cc3fd7b22
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-11-25 13:49:46 +0400

    feat: Extend mapping for compensating objects

--- order-service/src/main/java/com/ryzendee/orderservice/mapper/CommandMapper.java
+++ order-service/src/main/java/com/ryzendee/orderservice/mapper/CommandMapper.java
@@ -1,11 +1,16 @@
 package com.ryzendee.orderservice.mapper;
 
 import com.ryzendee.kafka.models.commands.order.ApproveOrderCommand;
+import com.ryzendee.kafka.models.commands.order.RejectOrderCommand;
+import com.ryzendee.kafka.models.commands.product.CancelProductReservationCommand;
+import com.ryzendee.kafka.models.commands.product.ProductReservationCancelledEvent;
 import com.ryzendee.kafka.models.commands.shipment.CreateShipmentCommand;
 import com.ryzendee.kafka.models.commands.payment.ProcessPaymentCommand;
 import com.ryzendee.kafka.models.commands.product.ReserveProductCommand;
 import com.ryzendee.kafka.models.events.order.OrderCreatedEvent;
+import com.ryzendee.kafka.models.events.payment.PaymentProcessFailedEvent;
 import com.ryzendee.kafka.models.events.payment.PaymentProcessedEvent;
+import com.ryzendee.kafka.models.events.product.ProductReservationFailedEvent;
 import com.ryzendee.kafka.models.events.product.ProductReservedEvent;
 import com.ryzendee.kafka.models.events.shipment.ShipmentCreatedEvent;
 import org.mapstruct.Mapper;
@@ -18,4 +23,8 @@ public interface CommandMapper {
     ProcessPaymentCommand map(ProductReservedEvent reservedEvent);
     CreateShipmentCommand map(PaymentProcessedEvent processedEvent);
     ApproveOrderCommand map(ShipmentCreatedEvent createdEvent);
+
+    CancelProductReservationCommand map(PaymentProcessFailedEvent failedEvent);
+    RejectOrderCommand map(ProductReservationFailedEvent failedEvent);
+    RejectOrderCommand map(ProductReservationCancelledEvent cancelledEvent);
 }
